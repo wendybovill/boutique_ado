87d66c42c6c04197015168796f70a77bb4c143c2
wendybovill <wendybovill@gmail.com>
2023-07-01 14:39:18 +0000

    added products templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-wendybovill-boutiqueado-pcal64olykp.ws-eu101.gitpod.io']
 
 
 # Application definition
