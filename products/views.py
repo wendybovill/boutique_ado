1ac7cdee919b140589bfd2f8c733b8f5c17208b6
wendybovill <wendybovill@gmail.com>
2023-07-01 18:16:14 +0000

    added sort select options box to products.html and to all_products views.py

--- products/views.py
+++ products/views.py
@@ -5,6 +5,7 @@ from .models import Product, Category
 
 # Create your views here.
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
@@ -21,7 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
